e744eae23d8bec55609072ea4bf0480031e7b590
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-03 13:14:10 -0400

    add NavBar component and placeholder texxt

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+      <nav className="navbar">
+        <div className="container">
+          NAV BAR
+        </div>
+      </nav>
+    )
+}
+export default NavBar;
