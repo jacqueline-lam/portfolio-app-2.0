847d28c8a55c12108f7b2255e354e3a6b2117f8f
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-04 17:37:32 -0400

    add Route to render/display BlogPostsContainer and add header to BlogPostsContainer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import ProjectsContainer from './containers/ProjectsContainer';
+import BlogPostsContainer from './containers/BlogPostsContainer'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Switch>
           {/* only first child <Route> that matches the location gets rendered */}
           {/* When URL matches specified path, render component */}
+          <Route path='/blog' component={BlogPostsContainer} />
           <Route path='/projects' component={ProjectsContainer} />
           <Route path='/about' component={About} />
           <Route exact path='/' component={Home} />
---
+++ src/containers/BlogPostsContainer.js
@@ -0,0 +1,9 @@
+function BlogPostsContainer() {
+  return (
+    <div id='blog-container' className='component'>
+      <h1>BLOG</h1>
+    </div>
+  )
+}
+
+export default BlogPostsContainer;
