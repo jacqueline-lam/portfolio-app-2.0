2109613eaef0dbdf5c63f30a31dfcb1787f9cdb2
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-04 15:58:50 -0400

    add NavLink components to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,59 @@
+// NavLink = superset of Link, adding styling attrs to rendered ele
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
   return (
       <nav className="navbar">
         <div className="container">
-          [NAV BAR]
+          <NavLink
+            className='navbar-brand'
+            to="/"
+            exact
+          >Jacqueline Lam</NavLink>
+          <div className="justify-content-end" id="navbarDefault" >
+            <ul className="navbar-nav">
+              <li className='nav-item'>
+                <NavLink
+                  className='nav-link'
+                  to="/"
+                  exact
+                  activeStyle={{
+                    color: '#bda38f'
+                  }}
+                >Home</NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  className='nav-link'
+                  to="/about"
+                  exact
+                  activeStyle={{
+                    color: '#bda38f'
+                  }}
+                >About</NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  className='nav-link'
+                  to="/projects"
+                  exact
+                  activeStyle={{
+                    color: '#bda38f'
+                  }}
+                >Portfolio</NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink
+                  className='nav-link'
+                  to="/blog"
+                  exact
+                  activeStyle={{
+                    color: '#bda38f'
+                  }}
+                >Blog</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
