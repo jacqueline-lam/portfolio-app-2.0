dd414b478a02c9fa8d75a96125b1f936141bc81a
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-09 14:24:46 -0500

    add project state and useEffect fn to fetch data from API

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,4 +1,14 @@
+import React, { useEffect, useState } from "react";
+
 function ProjectsContainer() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+      fetch("https://jlam-portfolio-backend.herokuapp.com/api/v1/projects")
+          .then(r => r.json())
+          .then(rJSON => setProjects(rJSON.projects))
+  }, [])
+
   return (
     <div id='projects-container' className='component'>
       <h1>PORTFOLIO</h1>
