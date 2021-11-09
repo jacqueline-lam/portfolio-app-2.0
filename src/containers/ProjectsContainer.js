478dd8873847fc148358f10609f6850bb9252c28
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-09 17:27:07 -0500

    add loading logic to ProjectsContainer

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 
 function ProjectsContainer() {
   const [projects, setProjects] = useState([]);
+  const [loadingProjects, setLoadingProjects] = useState(true);
 
   useEffect(() => {
       fetch("https://jlam-portfolio-backend.herokuapp.com/api/v1/projects")
           .then(r => r.json())
-          .then(rJSON => setProjects(rJSON.projects))
+          .then(rJSON => {
+            setProjects(rJSON.projects);
+            setLoadingProjects(false);
+          })
   }, [])
+
+  if (loadingProjects) {
+    return <h2>Loading...</h2>
+  }
 
   return (
     <div id='projects-container' className='component'>
       <h1>PORTFOLIO</h1>
       <h3>My Latest Projects</h3>
+      <p>projects</p>
     </div>
   )
 }
