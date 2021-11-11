09c796bab30f4b5d3fc266213ff79e67c23c7140
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-11-11 14:54:06 -0500

    debug img src and add simple app css to resize img

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,12 +1,14 @@
 import {Link} from 'react-router-dom';
 
 function ProjectCard ({ project }) {
+  console.log(project)
   const projectStacks = project.stacks.map(stack => stack.name).join(' | ');
 
   const fileName = project.image_url
   // special feature supported by webpack's compiler - to get all matching modules starting from some base directory
   const images = require.context('../../images', true)
   let img = images('./' + fileName)
+  console.log(img.default)
 
   return (
     <div id='project-list'>
@@ -14,7 +16,7 @@ function ProjectCard ({ project }) {
       <Link key={project.id} to={`/projects/${project.id}`}>
         <h4>{project.name}</h4>
       </Link>
-      <img className='project-thumbnail img-fluid' alt='project thumbnail' src={img} />
+      <img className='project-thumbnail img-fluid' alt='project thumbnail' src={img.default} />
       <i>{projectStacks}</i>
       <hr />
     </div >
