1f223c7802d0403032e9d09ba284af75ff5fc9d4
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2022-01-03 18:29:07 -0500

    add RRouter and useRouteMatch hook to Projects component and useParams to ProjectDetails for client-side routing

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+// Here we add `match` to the arguments so we can access the path info
+// in `routerProps` that is passed from ProjectCard.js
 
-function ProjectDetails(){
-  return(
+function ProjectDetails({ projects }) {
+  // call useParams to access the `params` from the url:
+  // the dynamic portion of our /projects/:projectId path
+  const params = useParams();
+  const project = projects.find(proj => proj.id.toString() === params.projectId)
+
+
+  return (
     <div className='project-details'>
-      Project Details
+      {project.name}
     </div>
   )
 }
--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+// import custom `useRouteMatch` from React Router
+import { Route, useRouteMatch } from 'react-router-dom';
 import ProjectFilters from '../components/projects/ProjectFilters';
 import ProjectList from '../components/projects/ProjectList';
 import ProjectDetails from '../components/projects/ProjectDetails';
@@ -7,23 +9,25 @@ function ProjectsContainer() {
   const [stacks, setStacks] = useState([]);
   const [projects, setProjects] = useState([]);
   const [loadingProjects, setLoadingProjects] = useState(true);
+  // useRouteMatch returns a special obj w/ info about currently matched route
+  const match = useRouteMatch();
 
   const fetchStacks = () => {
     fetch('https://jlam-portfolio-backend.herokuapp.com/api/v1/stacks')
-    .then(r => r.json())
-    .then(rJSON => {
-      setLoadingProjects(false);
-      setStacks(rJSON);
-    })
+      .then(r => r.json())
+      .then(rJSON => {
+        setLoadingProjects(false);
+        setStacks(rJSON);
+      })
   };
 
   const fetchProjects = () => {
     fetch("https://jlam-portfolio-backend.herokuapp.com/api/v1/projects")
-        .then(r => r.json())
-        .then(rJSON => {
-          setProjects(rJSON);
-          setLoadingProjects(false);
-        })
+      .then(r => r.json())
+      .then(rJSON => {
+        setProjects(rJSON);
+        setLoadingProjects(false);
+      })
   };
 
   // empty dependencies array in second arg, so only run fetch ONCE
@@ -39,6 +43,11 @@ function ProjectsContainer() {
   return (
     <div id='projects-container' className='component'>
       <h1>PORTFOLIO</h1>
+      {/*  we can use current URL from the `match` object as part of the path,
+        this will generate a url like "/projects/:projectId" */}
+      <Route path={`${match.url}/:projectId`}>
+        <ProjectDetails projects={projects} />
+      </Route>
       <>
         {
           loadingProjects ?
@@ -48,9 +57,9 @@ function ProjectsContainer() {
               <h3>My Latest Projects</h3>
               <ProjectFilters
                 stacks={stacks}
-                // onFilterChange={handleFiltersChange}
+              // onFilterChange={handleFiltersChange}
               />
-              <ProjectList projects={projects}/>
+              <ProjectList projects={projects} />
             </div>
         }
       </>
